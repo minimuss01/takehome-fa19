259e8688688f16f7fe41c2cb17839cd0e6fdc694
Charles <charlie.kuch@gmail.com>
2019-09-15 23:46:11 -0500

    Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Instructions from './Instructions'
 import Contact from './Contact'
+import Counter from "./Counter"
 
 class App extends Component {
   constructor(props) {
     super(props)
+    this.counter = React.createRef()
     this.state = {
       contacts: [
         {id: 1, name: "Angad", nickname: "greg", hobby: "dirty-ing"},
@@ -13,14 +15,37 @@ class App extends Component {
       ]
     }
   }
-
+  handleSubmit = event => {
+    event.preventDefault()
+    this.counter.current.handleInc()
+    let new_contact = {id: this.state.contacts.length+1, name: this.nodeName.value, nickname: this.nodeNickname.value, hobby: this.nodeHobby.value}
+    let updated_contacts = this.state.contacts;
+    updated_contacts.push(new_contact)
+    console.log(updated_contacts);
+    this.setState({contacts: updated_contacts});
+    this.nodeForm.reset()
+  }
   render() {
     return (
       <div className="App">
-        <Instructions />
-
+        <Instructions complete="true"/>
+        <Counter ref={this.counter}/>
+        <br />
+        <form onSubmit={this.handleSubmit} ref={node => (this.nodeForm = node)}>
+        <label>Name:
+          <input type="text" ref={node => (this.nodeName = node)}/>
+        </label> <br />
+        <label>Nickname:
+          <input type="text" ref={node => (this.nodeNickname = node)}/>
+        </label> <br />
+        <label>Hobby:
+          <input type="text" ref={node => (this.nodeHobby = node)}/>
+        </label> <br />
+        <button type="submit">Submit</button>
+      </form>
+        <br />
         {this.state.contacts.map(x => (
-          <Contact id={x.id} name={x.name} nickcname={x.nickname} hobby={x.hobby} />
+          <Contact id={x.id} name={x.name} nickname={x.nickname} hobby={x.hobby} />
         ))}
       </div>
     )
--- frontend/src/Counter.js
+++ frontend/src/Counter.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
   // YOUR CODE GOES BELOW
-  
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+  handleInc = () => {
+    this.setState({count: this.state.count + 1})
+  }
+  handleDec = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
   render() {
     return (
-      <div/>
+      <div>
+        <p>Counter</p>
+        <label>{this.state.count}</label>
+        <br></br>
+        <button onClick={this.handleInc}>Increment</button>
+        <button onClick={this.handleDec}>Decrement</button>
+      </div>
     )
   }
 }
